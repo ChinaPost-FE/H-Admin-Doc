fabbb70be7f36ca8c14fbbe16ccb00d497b8855b
Ronny_Yip <leaves18@qq.com>
2018-07-05 08:19:12 +0800

    update

--- src/js/demo/doc.js
+++ src/js/demo/doc.js
@@ -1,32 +1,35 @@
-$('.fa-copy').on('click', function() {
-    window.toastr.info('已复制代码到粘贴板!')
-    // document.execCommand('Copy')
-})
+$('.fa-copy').on('click', () => {
+  window.toastr.info('已复制代码到粘贴板!');
+  // document.execCommand('Copy')
+});
 
 function renderTpl(html) {
-    return `<div class="code-wrapper">${html}</div>`
+  return `<div class="code-wrapper">${html}</div>`;
 }
 
 function htmlEncode(html) {
-    const temp = document.createElement('div')
-    temp.textContent != null ? (temp.textContent = html) : (temp.innerText = html)
-    return temp.innerHTML.replace(/^\s+/, '')
+  const temp = document.createElement('div');
+  temp.textContent != null ? (temp.textContent = html) : (temp.innerText = html);
+  return temp.innerHTML.replace(/^\s+/, '');
 }
 
-$('.fa-code').on('click', function() {
-    const html = style_html($(this).closest('.ibox')[0].innerHTML)
-    // console.log(html)
+$('.fa-code').on('click', function () {
+  const html = style_html($(this)
+    .closest('.ibox')
+    .eq(0)
+    .prop('outerHTML'));
+  // console.log(html)
 
-    layer.open({
-        type: 1,
-        title: false,
-        closeBtn: 0,
-        area: '820px',
-        skin: 'layui-layer-nobg', //没有背景色
-        shadeClose: true,
-        content: renderTpl('<pre><code class="html">' + htmlEncode(html) + '</code></pre>'),
-        success: function(ctx) {
-            hljs.highlightBlock(ctx.find('pre code')[0])
-        }
-    })
-})
+  layer.open({
+    type: 1,
+    title: false,
+    closeBtn: 1,
+    area: '820px',
+    skin: 'layui-layer-nobg', // 没有背景色
+    shadeClose: true,
+    content: renderTpl(`<pre><code class="html">${htmlEncode(html)}</code></pre>`),
+    success(ctx) {
+      hljs.highlightBlock(ctx.find('pre code')[0]);
+    },
+  });
+});
